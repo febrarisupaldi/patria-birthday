9ff6abb9af7a01eb9730022e66d34e91fc71715b
Febrari Supaldi <febrarisupaldi@outlook.com>
2024-04-23 09:24:33 +0700

    add fira as receipent email

--- app/Http/Controllers/PatriaFriendController.php
+++ app/Http/Controllers/PatriaFriendController.php
@@ -13,39 +13,44 @@ class PatriaFriendController extends Controller
 {
     private PatriaFriendService $patriaFriendService;
 
-    public function __construct(PatriaFriendService $patriaFriendService){
+    public function __construct(PatriaFriendService $patriaFriendService)
+    {
         $this->patriaFriendService = $patriaFriendService;
     }
 
-    public function index(Request $request):JsonResponse{
+    public function index(Request $request): JsonResponse
+    {
         $data = $this->patriaFriendService->getAllPatriaFriend();
-        return response()->json(["status"=>"ok","message"=>"ok","data"=>$data],200);
+        return response()->json(["status" => "ok", "message" => "ok", "data" => $data], 200);
     }
 
-    public function showTodayBirthday():JsonResponse{
+    public function showTodayBirthday(): JsonResponse
+    {
         $data = $this->patriaFriendService->showTodayBirthday();
-        if(count($data) == 0)
+        if (count($data) == 0)
             $data = [];
-        return response()->json(["status"=>"ok","message"=>"ok","data"=>$data],200);
+        return response()->json(["status" => "ok", "message" => "ok", "data" => $data], 200);
     }
 
-    public function sendEmailNotif():JsonResponse{
+    public function sendEmailNotif(): JsonResponse
+    {
         $data = $this->patriaFriendService->showTodayBirthday();
-        if(count($data) > 0){
+        if (count($data) > 0) {
             try {
-                foreach(['febsupaldi@gmail.com','nopiahirawan26@gmail.com'] as $receipent){
+                foreach (['febsupaldi@gmail.com', 'nopiahirawan26@gmail.com', 'firalstr30@gmail.com'] as $receipent) {
                     Mail::to($receipent)->send(new NotifBirthdayMail($data));
                 }
-                return response()->json(["status"=>"ok","message"=>"ok","data"=>$data],200);
+                return response()->json(["status" => "ok", "message" => "ok", "data" => $data], 200);
             } catch (\Throwable $th) {
-                return response()->json(["status"=>"error","message"=>"error","data"=>$data], 500);
+                return response()->json(["status" => "error", "message" => "error", "data" => $data], 500);
             }
         }
 
-        return response()->json(["status"=>"error","message"=>"error","data"=>"not found"], 200);
+        return response()->json(["status" => "error", "message" => "error", "data" => "not found"], 200);
     }
-    public function saveFriend(StoreFriendRequest $request):JsonResponse{
+    public function saveFriend(StoreFriendRequest $request): JsonResponse
+    {
         $data = $this->patriaFriendService->storePatriaFriend($request);
-        return response()->json(["status"=>"ok","message"=>$data],200);
+        return response()->json(["status" => "ok", "message" => $data], 200);
     }
 }
